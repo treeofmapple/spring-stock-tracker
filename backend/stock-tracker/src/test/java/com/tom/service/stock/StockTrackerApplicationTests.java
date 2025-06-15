5774cdc630f10c03f87e1369adb166b9ca66216b
Samuel Pereira <samuelsantana778@gmail.com>
2025-06-15 02:14:20 -0300

    folder update

---
+++ backend/stock-tracker/src/test/java/com/tom/service/stock/StockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.service.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
